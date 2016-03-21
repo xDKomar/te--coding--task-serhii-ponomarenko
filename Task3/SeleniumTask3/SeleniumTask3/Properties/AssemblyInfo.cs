e4f2686eb891cdcde719519506f878834c1ab937
xdkomar <xdkomar@gmail.com>
2016-03-22 01:25:29 +0200

    Task3

--- Task3/SeleniumTask3/SeleniumTask3/Properties/AssemblyInfo.cs
+++ Task3/SeleniumTask3/SeleniumTask3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SeleniumTask1")]
+[assembly: AssemblyTitle("SeleniumTask3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SeleniumTask1")]
+[assembly: AssemblyProduct("SeleniumTask3")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b7c7cf58-4cf9-4342-9df0-e3dd502e113d")]
+[assembly: Guid("b9ea2c31-6058-459e-89d8-d0e3cc070bda")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Task3/SeleniumTask3/SeleniumTask3/TestClass.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTask1
+{
+    [TestFixture()]
+    public class TestClass
+    {
+        private ChromeDriver _driver;
+
+        [SetUp]
+        public void Init()
+        {
+            _driver = new ChromeDriver();
+        }
+
+
+        [Test]
+        public void HomeItemTest()
+        {
+            _driver.Navigate().GoToUrl("http://zalando-edinc.rhcloud.com/shifting_content/menu");
+            _driver.FindElement(By.LinkText("Home")).Click();
+
+            Assert.AreEqual(_driver.Url, "http://zalando-edinc.rhcloud.com/");
+        }
+
+        [Test]
+        public void AboutTest()
+        {
+            _driver.Navigate().GoToUrl("http://zalando-edinc.rhcloud.com/shifting_content/menu");
+            _driver.FindElement(By.LinkText("About")).Click();
+
+            Assert.AreEqual(_driver.Url, "http://zalando-edinc.rhcloud.com/about");
+        }
+
+        [Test]
+        public void ContactUsTest()
+        {
+            _driver.Navigate().GoToUrl("http://zalando-edinc.rhcloud.com/shifting_content/menu");
+            _driver.FindElement(By.LinkText("Contact Us")).Click();
+
+            Assert.AreEqual(_driver.Url, "http://zalando-edinc.rhcloud.com/contact-us");
+        }
+
+        [Test]
+        public void PortfolioTest()
+        {
+            _driver.Navigate().GoToUrl("http://zalando-edinc.rhcloud.com/shifting_content/menu");
+            _driver.FindElement(By.LinkText("Portfolio")).Click();
+
+            Assert.AreEqual(_driver.Url, "http://zalando-edinc.rhcloud.com/portfolio");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
